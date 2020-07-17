2f3ee96ce107876c948079321f2af98c2b30919c
Gregor Pirolt <gregor.pirolt@me.com>
2020-07-17 16:42:45 +0200

    remove plugin-related interfaces to separate project

--- iGrgdPlugin.go
+++
@@ -1,8 +0,0 @@
-package interfaces
-
-// IGrgdPlugin ...
-type IGrgdPlugin interface {
-	Init(i interface{}) interface{}
-	GetMetaData(i interface{}) interface{}
-	Methods(i interface{}) map[string]interface{}
-}
